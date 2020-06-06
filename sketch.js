f0d6b5d2a25b528a1c3ef2c27e69f96091108d03
Diyadnair <64205177+Diyadnair@users.noreply.github.com>
2020-06-06 16:27:51 +0530

    Add files via upload
    paper, dustbin

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbinObject,paperObject,groundObject
+var world;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	dustbinObject = new Dustbin(1200,650);
+	paperObject = new Paper(200,450,40);
+	groundObject = new Ground(width/2,670,width,20);
+	
+	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	//World.add(world, packageBody);
+	//Create a Ground
+	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 //World.add(world, ground);
+
+	//Create a ground
+
+
+	Engine.run(engine);
+	  //Render.run(render);
+	  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  dustbinObject.display();
+  paperObject.display();  
+  groundObject.display();
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	
+	}
+}
+
+
